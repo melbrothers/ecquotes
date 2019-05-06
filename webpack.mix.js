5a43c8d418c7f13eeede4908dc9daafa4e01d79f
Zheng Xin <jason.zhengxin@gmail.com>
2019-05-06 15:56:30 +1000

    update the gitignore and webpack mix

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,7 +12,6 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-   .css('resources/css/argon.css', 'public/css')
    .sass('resources/sass/app.scss', 'public/css')
    .stylus('resources/stylus/app.styl', 'public/css')
    .disableNotifications()
