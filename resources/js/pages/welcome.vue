af19dfd6b070b867984639c4b860e42d2bf7287a
Zheng Xin <jason.zhengxin@gmail.com>
2019-06-08 14:48:59 +1000

    fixing the mobile phone issue

--- resources/js/pages/welcome.vue
+++ resources/js/pages/welcome.vue
@@ -6,9 +6,9 @@
         <v-container fluid class="pa-0">
           <v-layout row justify-center>
             <v-flex xs12>
-              <div class="sign-up-box">
-                <h1>Compare. Request. Order. Manage</h1>
-                <h2>Your one stop shop for all your ordering needs completely FREE.</h2>
+              <div class="sign-up-box blue darken-4 pa-4">
+                <h1 class="white--text font-weight-500 display-2">Compare. Request. Order. Manage</h1>
+                <h2 class="white--text font-weight-500 title mt-3">Your one stop shop for all your ordering needs completely FREE.</h2>
                  <v-btn class="signup-btn mt-4">Sign up</v-btn>
               </div>
             </v-flex>
@@ -16,8 +16,8 @@
           <v-layout row wrap class="intro-box">
             <v-flex md8 align-self-center>
               <div class="px-5 py-3">
-                <h1 >Trusted within the industry.</h1>
-                <h2>Save time and money by ordering and requesting quotes from the biggest range of electrical wholesalers and products within Australia.</h2>
+                <h1 class="display-3 font-weight-bold indigo--text text-darken-4">Trusted within the industry.</h1>
+                <h2 class="title indigo--text text-darken-4">Save time and money by ordering and requesting quotes from the biggest range of electrical wholesalers and products within Australia.</h2>
               </div>
 
             </v-flex>
@@ -28,29 +28,29 @@
               </div>
             </v-flex>
           </v-layout>
-          <v-layout row text-xs-center class="intro-box bg-color-1 ma-4">
+          <v-layout row text-xs-center class="intro-box grey lighten-3 ma-4">
             <v-flex xs12 align-self-center>
               <div class="recommendation pa-4">
                 <h1>It's an amazing tool I use on a day to day basis. Simplicity and the amount of time saved it crucial for my business." <br>- John Snow</h1>
               </div>
             </v-flex>
           </v-layout>
-          <v-layout class="intro-box bg-color-2 ma-4">
+          <v-layout class="intro-box blue lighten-1 ma-4">
             <v-flex md8 align-self-center>
               <div class="pa-4">
-                <h1>Compare Electrical Products: </h1>
-                <p>Easily find and compare items and prices from the biggest range of electrical products in on location.</p>
+                <h1 class="white--text display-3 font-weight-bold">Compare Electrical Products: </h1>
+                <p class="white--text subheading">Easily find and compare items and prices from the biggest range of electrical products in on location.</p>
               </div>
             </v-flex>
             <v-flex md4 hidden-xs-only align-self-center>
                 <v-img src="/images/comEl.png" alt="compare image" ></v-img>
             </v-flex>
           </v-layout>
-          <v-layout row class="intro-box bg-color-3 ma-4">
+          <v-layout row class="intro-box cyan lighten-2 ma-4">
             <v-flex md8 align-self-center>
               <div class="pa-4">
-                <h1>Request Quote: </h1>
-                <p>Quotes can be sent out to every wholesaler in one click, saving you time from sending the same order out to each individual suppliers.</p>
+                <h1 class="white--text display-3 font-weight-bold">Request Quote: </h1>
+                <p class="white--text subheading">Quotes can be sent out to every wholesaler in one click, saving you time from sending the same order out to each individual suppliers.</p>
               </div>
             </v-flex>
             <v-flex md4 hidden-xs-only align-self-center>
@@ -58,33 +58,33 @@
             </v-flex>
 
           </v-layout>
-          <v-layout row justify-center class="intro-box bg-color-4 ma-4">
+          <v-layout row justify-center class="intro-box green darken-3 ma-4">
             <v-flex md8 align-self-center>
               <div class="pa-4">
-                <h1>Reverse Actions: </h1>
-                <p>Wholesalers have the option to bid for the lowest price for an order you have requested.</p>
+                <h1 class="white--text display-3 font-weight-bold">Bidding: </h1>
+                <p class="white--text subheading">Wholesalers have the option to bid for the lowest price for an order you have requested.</p>
               </div>
             </v-flex>
             <v-flex md4 hidden-xs-only align-self-center>
               <v-img src="/images/bid.png" alt="bid image"></v-img>
             </v-flex>
           </v-layout>
-          <v-layout row justify-center class="intro-box bg-color-5 ma-4">
+          <v-layout row justify-center class="intro-box orange darken-4 ma-4">
             <v-flex md8 align-self-center>
               <div class="pa-4">
-                <h1>Order management: </h1>
-                <p>Manage all incoming orders, transactions and jobs in one simple platform, giving you the ability to keep track of orders and prices, and easily chase up on any issues you have with your orders.</p>
+                <h1 class="white--text display-3 font-weight-bold">Order management: </h1>
+                <p class="white--text subheading">Manage all incoming orders, transactions and jobs in one simple platform, giving you the ability to keep track of orders and prices, and easily chase up on any issues you have with your orders.</p>
               </div>
             </v-flex>
             <v-flex md4 hidden-xs-only align-self-center>
               <v-img src="/images/orderMGM.png" alt="order image"></v-img>
             </v-flex>
           </v-layout>
-          <v-layout row justify-center class="intro-box bg-color-6 ma-4">
+          <v-layout row justify-center class="intro-box green lighten-2 ma-4">
             <v-flex md8 align-self-center>
               <div class="pa-4">
-                <h1>Employee Integration: </h1>
-                <p>Integrate your employees with this system and manage all of their orders and jobs in one place.</p>
+                <h1 class="white--text display-3 font-weight-bold">Employee Integration: </h1>
+                <p class="white--text subheading font-weight-medium">Integrate your employees with this system and manage all of their orders and jobs in one place.</p>
               </div>
             </v-flex>
             <v-flex md4 hidden-xs-only align-self-center>
@@ -92,7 +92,7 @@
             </v-flex>
           </v-layout>
 
-          <h1 class="ma-2 display-3">We differ from other services</h1>
+          <h1 class="ma-2 display-2 font-weight-bold">We differ from other services</h1>
           <v-layout row wrap>
             <v-flex md6 xs12 sm6 d-flex class="my-1">
               <v-card class="d-flex">
@@ -165,9 +165,9 @@
             </v-flex>
           </v-layout>
           <v-layout row wrap>
-            <v-flex class="bottom-box" md7 xs12>
-                <h1>Be a part of the <span>future</span> </h1>
-                <p>Sign up today to access the biggest range of eletrical wholesalers and products within Australia.</p>
+            <v-flex class="blue darken-4 white--text py-2 px-5" md7 xs12>
+                <h1 class="display-3 font-weight-bold">Be a part of the <span>future</span> </h1>
+                <p class="subheading">Sign up today to access the biggest range of eletrical wholesalers and products within Australia.</p>
                 <v-btn class="signup-btn mt-4">Sign up</v-btn>
             </v-flex>
             <v-flex class="bottom-img" md5 hidden-xs-only>
@@ -200,75 +200,23 @@ export default class Welcome extends Vue {
   .sign-up-box, .intro-box {
     display: flex;
     min-height: 400px;
-    &.bg-color-1 {
-      background: #e5e5e5;
-    }
-    &.bg-color-2 {
-      background: var(--v-secondary-base);
-      h1, h2, p {
-        color: #fff;
-      }
-    }
-    &.bg-color-3 {
-      background: var(--v-accent-base);
-      h1, h2, p {
-        color: #fff;
-      }
-    }
-    &.bg-color-4 {
-      background: var(--v-success-base);
-      h1, h2, p {
-        color: #fff;
-      }
-    }
-    &.bg-color-5 {
-      background: var(--v-info-base);
-      h1, h2, p {
-        color: #fff;
-      }
-    }
-    &.bg-color-6 {
-      background: var(--v-warning-base);
-      h1, h2, p {
-        color: #fff;
-      }
-    }
   }
   .sign-up-box{
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    background: var(--v-primary-base);
     h1, h2 {
-      color: #fff;
-      font-weight: 500;
       text-shadow: 1px 0px 4px #000000;
     }
     h1 {
-      font-size: 6vw;
       word-spacing: 10px;
     }
-    h2 {
-      font-size: 2rem;
-    }
+  
   }
   .intro-box {
-    background: #fff;
     align-items: flex-start;
     justify-content: flex-start;
-    h1, h2 {
-      font-weight: 500;
-    }
-    h1 {
-      font-size: 5rem;
-      color: var(--v-primary-base);
-    }
-    h2 {
-      font-size: 2rem;
-    }
-    p {
-      font-size: 1.2rem;
-    }
+  
     .intro-img {
       display: flex;
       flex-direction: column;
@@ -307,9 +255,6 @@ export default class Welcome extends Vue {
     }
   }
 
-  .box-1 {
-    color: #fff;
-  }
   .responsive-img {
     width: 100%;
     height: 100%;
