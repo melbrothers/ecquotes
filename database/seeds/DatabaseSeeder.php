42c3e632a87d592a44dc328052be9402723b0a30
Lixing <zlxjackie@hotmail.com>
2019-07-07 21:40:37 +1000

    Fix migratioin

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,40 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        DB::table('oauth_clients')->insert([
+            [
+              "id"=> 1,
+              "user_id"=> null,
+              "name"=>  "Laravel Personal Access Client",
+              "secret"=>  "sW0mBS2yjFMrQFGXUlQYdJI42fDcXyIwb4ZigJJf",
+              "redirect"=>  "http://localhost",
+              "personal_access_client"=>  1,
+              "password_client"=> 0,
+              "revoked"=>  0,
+              "created_at"=>  "2019-05-09 11:58:24",
+              "updated_at"=>  "2019-05-09 11:58:24"
+            ],
+            [
+              "id"=>  2,
+              "user_id"=>  null,
+              "name"=>  "Laravel Password Grant Client",
+              "secret"=>  "b4XM65hFh7nj3EYxWYFer0ypTF3jwbS97zIH0nn2",
+              "redirect" =>  "http://localhost",
+              "personal_access_client" =>  0,
+              "password_client"=>  1,
+              "revoked" =>  0,
+              "created_at"=>  "2019-05-09 11:58:24",
+              "updated_at"=>  "2019-05-09 11:58:24"
+            ]
+          ]);
+
+          DB::table('oauth_personal_access_clients')->insert([
+            [
+                "id"=> 1,
+                "client_id"=> 1,
+                "created_at"=> "2019-05-09 11:58:24",
+                "updated_at"=> "2019-05-09 11:58:24"
+              ]
+          ]);
     }
 }
