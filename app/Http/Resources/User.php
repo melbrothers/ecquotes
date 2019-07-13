291a5d338adacc11936460ada012d393cf42042f
Lixing <zlxjackie@hotmail.com>
2019-07-13 19:18:33 +1000

    Add user resource

---
+++ app/Http/Resources/User.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class User extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->id,
+            'email' => $this->email,
+            'first_name' => $this->first_name,
+            'last_name' => $this->last_name,
+            'gender' => $this->gender,
+            'dob' => $this->dob,
+            'abn' => $this->abn,
+            'licence' => $this->licence,
+        ];
+    }
+}
